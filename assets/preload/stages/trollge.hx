d20547fc8b6fb584ac85eb02fa41e7810ff3dd1f
TheLeerName <lesha.nechaev.05@mail.ru>
2023-08-03 18:57:11 +0700

    rewrite trollge stage script

--- assets/preload/stages/trollge.hx
+++ assets/preload/stages/trollge.hx
@@ -1,8 +1,29 @@
 import openfl.geom.Point;
-import hxcodec.flixel.FlxVideoSprite; // u cant use this without my pr!!!! https://github.com/ShadowMario/FNF-PsychEngine/pull/12953/
+import hxcodec.flixel.FlxVideo;
 import lime.utils.Log;
 
 Log.throwErrors = false; // avoid crash on shader errors (im tired from restarting game when i do shaders)
+
+var modchartVideos = [];
+var modchartVideosFocusGainedCallbacks = [];
+function loadVideoGraphic(sprite:FlxSprite, path:String, shouldLoop:Bool = false) {
+	var video = new FlxVideo();
+	video.alpha = 0;
+	video.play(Paths.video(path), shouldLoop);
+	video.onTextureSetup.add(() -> {
+		sprite.loadGraphic(video.bitmapData);
+	});
+
+	FlxG.signals.focusGained.remove(video.resume);
+	var callback = () -> {
+		if (!game.paused) video.resume();
+	};
+	modchartVideosFocusGainedCallbacks.push(callback);
+	FlxG.signals.focusGained.add(callback);
+
+	modchartVideos.push(video);
+	FlxG.game.addChild(video);
+}
 
 var bg:FlxSprite = new FlxSprite();
 var fadeshader:FlxRuntimeShader = new FlxRuntimeShader("
@@ -22,7 +43,7 @@ var chromakeyshader = new FlxRuntimeShader("
 		gl_FragColor.rgb /= 1.5; // fade
 	}
 ");
-var trollgeidle:FlxVideoSprite = new FlxVideoSprite();
+var trollgeidle:FlxSprite = new FlxSprite();
 
 function onCreatePost() {
 	bg.setPosition(70, -35);
@@ -33,14 +54,28 @@ function onCreatePost() {
 	game.dad.shader = game.gf.shader = game.boyfriend.shader = fadeshader;
 
 	trollgeidle.setPosition(game.gf.x - 160, game.gf.y - 30);
-	trollgeidle.play(Paths.video('trollgeidle'), true);
 	trollgeidle.shader = chromakeyshader;
+	loadVideoGraphic(trollgeidle, 'trollgeidle', true);
 	addBehindGF(trollgeidle);
+}
+function onDestroy() {
+	for (callback in modchartVideosFocusGainedCallbacks) FlxG.signals.focusGained.remove(callback);
+	for (video in modchartVideos) if (video != null)
+	{
+		video.dispose();
+
+		if (FlxG.game.contains(video))
+			FlxG.game.removeChild(video);
+	}
 }
 
 function onPause() {
-	trollgeidle.pause();
+	for (video in modchartVideos) {
+		video.pause();
+	}
 }
 function onResume() {
-	trollgeidle.resume();
+	for (video in modchartVideos) {
+		video.resume();
+	}
 }
