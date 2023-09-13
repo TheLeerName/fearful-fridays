1490436101c588c63dbcd97fb865f1a269e59b02
TheLeerName <lesha.nechaev.05@mail.ru>
2023-09-14 02:38:42 +0700

    gpu caching bugs jumpscare!!!!!!!!!

--- assets/preload/stages/NewMexico.lua
+++ assets/preload/stages/NewMexico.lua
@@ -7,4 +7,30 @@ function onCreatePost()
 	setProperty('dad.useFramePixels', true)
 	setSpriteShader('dad', 'curve')
 	setShaderFloat('dad', 'curveX', 0.05)
+
+	-- gpu caching bugs jumpscare!!!!!!!!!
+	runHaxeCode([[
+		import openfl.Lib;
+		import options.OptionsState;
+		import backend.MusicBeatState;
+
+		if (ClientPrefs.data.cacheOnGPU) {
+			FlxG.fullscreen = false;
+			for (cam in FlxG.cameras.list)
+				cam.alpha = 0;
+			Lib.application.window.alert('bro u have gpu caching enabled, pls disable it!!!!!!!!\n\nSwitching to Options Menu...', 'ERROR');
+
+			var state = new OptionsState();
+			MusicBeatState.switchState(state);
+			OptionsState.onPlayState = true;
+			if (state.options.length > 3)
+				OptionsState.curSelected = 3;
+
+			if(ClientPrefs.data.pauseMusic != 'None')
+			{
+				FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.data.pauseMusic)));
+				FlxTween.tween(FlxG.sound.music, {volume: 1}, 0.8);
+			}
+		}
+	]])
 end
